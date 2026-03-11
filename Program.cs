d16a9d8245265f1a288cbaf3b15711fd801af28d
manciolove <114284283+manciolove@users.noreply.github.com>
2026-03-11 12:50:59 +0100

    aggiunta es 6

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace matrici1
+﻿using System;
+
+namespace matrici1
 {
     internal class Program
     {
@@ -67,64 +69,91 @@
         // funzione esercizio 5
         static bool ControlloUguale(int[,] m)
         {
-            bool controllo = false;
-            int x = 6;
+            int x = 7;
             for (int i = 0; i < m.GetLength(0); i++)
             {
-                for ( int j=0; j < m.GetLength(1); j++)
+                for (int j = 0; j < m.GetLength(1); j++)
                 {
-                    if (m[i, j] != x)
+                    if (m[i,j] == x)
                     {
-                        controllo = false;
-                        i = m.GetLength(0);
-                        j = m.GetLength(1);
+                        return true;
                     }
                 }
-                if (controllo == true )
+            }
+            return false;
+        }
+
+        // funzione esercizio 6 
+        static int[,] Copia(int[,] m)
+        {
+            int[,] copia = new int[m.GetLength(0), m.GetLength(1)];
+            for (int i = 0; i < copia.GetLength(0); i++)
+            {
+                for (int j = 0; j < copia.GetLength(1); j++)
                 {
-                    return true;
-                }
-                else
-                {
-                    return false;
+                            copia[i, j] = m[i, j];
                 }
             }
+            return copia;
+        }
+
         static void Main(string[] args)
         {
             // esercizio 1
-            Console.WriteLine("Esercizio 1");
             Console.WriteLine("---------------");
-            int[,] m = { {5, 3}, {2 , 4}, {7 , 4} };
+            Console.WriteLine("Esercizio 1");
+            int[,] m = { { 5, 3 }, { 2, 4 }, { 7, 4 } };
             int somma = 0;
-            Console.WriteLine(Somma(m, somma));
+            Console.WriteLine("risultato: " + Somma(m, somma));
+            Console.WriteLine("---------------");
             Console.WriteLine();
 
             // esercizio 2
-            Console.WriteLine("Esercizio 2");
             Console.WriteLine("---------------");
+            Console.WriteLine("Esercizio 2");
             float media = 0;
-            Console.WriteLine(Media(m, media));
+            Console.WriteLine("risultato: " + Media(m, media));
+            Console.WriteLine("---------------");
             Console.WriteLine();
 
             // esercizio 3
-            Console.WriteLine("Esercizio 3");
             Console.WriteLine("---------------");
-            int min = 1000;
-            Console.WriteLine(Minimo(m));
+            Console.WriteLine("Esercizio 3");
+            Console.WriteLine("risultato: " + Minimo(m));
+            Console.WriteLine("---------------");
             Console.WriteLine();
 
             // esercizio 4
-            Console.WriteLine("Esercizio 4");
             Console.WriteLine("---------------");
-            int max = 0;
-            Console.WriteLine(Massimo(m));
+            Console.WriteLine("Esercizio 4");
+            Console.WriteLine("risultato: " + Massimo(m));
+            Console.WriteLine("---------------");
             Console.WriteLine();
 
             // esercizio 5
+            Console.WriteLine("---------------");
+            Console.WriteLine("Esercizio 5");
+            Console.WriteLine("risultato: " + ControlloUguale(m));
+            Console.WriteLine("---------------");
+            Console.WriteLine();
 
+            // esericizio 6
+            Console.WriteLine("---------------");
+            Console.WriteLine("Esercizio 6");
 
-
+            int[,] m2 = Copia(m);
+            for (int i = 0; i < m2.GetLength(0); i++)
+            {
+                for (int j = 0; j < m2.GetLength(1); j++)
+                {
+                    Console.Write("[" + m2[i, j] + "]");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("---------------");
+            Console.WriteLine();
 
         }
+
     }
 }
